b644f36f2278e53898756d90f8e3fa46d21b0671
tosello.daniel@gmail.com <tosello.daniel@gmail.com@f1aeea23-cc95-fa3a-c709-5a74fcc4fad4>
2011-11-02 04:42:10 +0000

    Modified Directory structure altered absolute paths to relative to consolidate location added update-handler-configs-demo as example startup files, sans paths and credentials

--- pipelines/xproc/get-vamas-xml.sh
+++ pipelines/xproc/get-vamas-xml.sh
@@ -2,8 +2,8 @@
 # $1=fedora-username
 # $2=fedora-password
 # $3=vamas URI
-rm /tmp/vamas-xml.xml
-rm /tmp/vamas.vms
-curl -u $1:$2 $3 -o /tmp/vamas.vms > /dev/null
-java -jar /ands/xproc/vamas-parser.jar /tmp/vamas.vms > /tmp/vamas-xml.xml
+rm ../tmp/vamas-xml.xml
+rm ../tmp/vamas.vms
+curl -u $1:$2 $3 -o ../tmp/vamas.vms > /dev/null
+java -jar ../xproc/vamas-parser.jar ../tmp/vamas.vms > ../tmp/vamas-xml.xml
 
