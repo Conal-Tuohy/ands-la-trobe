99d3effaadbf4b2cc61c7744f77496b45d30da17
tosello.daniel@gmail.com <tosello.daniel@gmail.com@f1aeea23-cc95-fa3a-c709-5a74fcc4fad4>
2011-12-02 00:48:22 +0000

    tart & stop scripts for calabash pipelines

---
+++ trunk/pipelines/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script starts a fedora-update-handler service
+
+# The service is a JMS client which listens to "fedora.apim.update" messages from Fedora.
+# When an update event occurs, the fedora-update-handler executes an external program
+# and passes the event message (an Atom XML document) to the program's standard input.
+while true
+do
+	java -jar fedora-update-handler/fedora-update-handler.jar start $1
+	echo "Restarting in 5 seconds. Press [CTRL+C] to stop.."
+	sleep 5
+done
+
