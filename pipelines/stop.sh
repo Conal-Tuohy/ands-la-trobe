b05eed541b03722843c082b18735160c7d080d71
tosello.daniel@gmail.com <tosello.daniel@gmail.com@f1aeea23-cc95-fa3a-c709-5a74fcc4fad4>
2011-12-02 00:48:22 +0000

    tart & stop scripts for calabash pipelines

---
+++ pipelines/stop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script stops a persistent subscription previously made by a fedora-update-handler service
+
+# The service is a JMS client which listens to "fedora.apim.update" messages from Fedora.
+# When an update event occurs, the fedora-update-handler executes an external program
+# and passes the event message (an Atom XML document) to the program's standard input.
+
+java -jar fedora-update-handler/fedora-update-handler.jar stop $1
